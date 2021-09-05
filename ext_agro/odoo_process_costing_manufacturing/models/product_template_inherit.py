716568e53710ed19f72221879f545a0042a82e61
solucionestecnoweb <inmldrsolucionestecnologicas@gmail.com>
2021-09-05 11:24:18 -0400

    Limpieza de repositorio

--- ext_agro/odoo_process_costing_manufacturing/models/product_template_inherit.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class Productos(models.Model):
-    _inherit = 'product.template'
-
-    producto_terminado = fields.Selection([('s','Si'),('n','No')])
